47f6f735da7e7902150d174f29c815ea6c27a654
b <biro123@ntlworld.com>
2017-09-10 23:58:09 +0100

    PC09 - Added a fade-in to the start scene, and corrected spelling error in title

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public float fadeInTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Image>().CrossFadeColor(new Color(0, 0, 0, 0), fadeInTime, false, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeSinceLevelLoad > fadeInTime)
+        {
+            gameObject.SetActive(false);
+        }    
+	}
+}
