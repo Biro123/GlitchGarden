5ccec312fe807a72f9346748fd25065c0d133304
b <biro123@ntlworld.com>
2017-09-27 00:11:57 +0100

    PC29 - Updated shooter to only attack when an enemy is in the lane

--- Assets/Scripts/Defenders/Shooter.cs
+++ Assets/Scripts/Defenders/Shooter.cs
@@ -6,9 +6,13 @@ public class Shooter : MonoBehaviour {
 
     public GameObject projectile, gun;
     private GameObject projectileParent;
-
+    private Animator animator;
+    private Spawner myLaneSpawner;
+    
     private void Start()
     {
+        animator = GetComponent<Animator>();
+
         // Create a parent to collect all spawned projectiles under - if not already existing.
         projectileParent = GameObject.Find("Projectiles");
         
@@ -16,8 +20,60 @@ public class Shooter : MonoBehaviour {
         {
             projectileParent = new GameObject("Projectiles");
         }
+
+        SetMyLaneSpawner();
+
     }
 
+    private void Update()
+    {
+        if ( IsAttackerInLane() )
+        {
+            animator.SetBool("isAttacking", true);
+        }    
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
+    }
+
+    private bool IsAttackerInLane()
+    {
+        Attacker[] attackerArray = myLaneSpawner.GetComponentsInChildren<Attacker>();
+
+        foreach (Attacker currAttacker in attackerArray)
+        {
+            if (currAttacker.gameObject.transform.position.x > this.transform.position.x)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    // Look through all spawners and set myLaneSpawner
+    private void SetMyLaneSpawner()
+    {
+        float thisPos = this.gameObject.transform.position.y;
+        Spawner[] spawnerArray = FindObjectsOfType<Spawner>();
+
+        foreach (Spawner currSpawner in spawnerArray)
+        {
+            if (thisPos == currSpawner.transform.position.y)
+            {
+                myLaneSpawner = currSpawner;
+                return;
+            }
+        }
+        
+        // Spwaner not found (see 'return' above)
+        Debug.LogError("Spawner not found in lane " + thisPos.ToString());
+        myLaneSpawner = null;
+    }
+
+    // Called from the animations
     private void Fire()
     {
         GameObject newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity);
